a8632e57273c8eab2b989d6a377e0517a07ba2aa
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-03 14:28:53 -0800

    add useEffect, API call

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 import Product from "../Product/Product";
+import styles from "./Shop.module.css";
 
 export default function Shop() {
-    const product = { id: 1, title: "Alex", rating: {rate: 6.9}, price: '$450', image: 'https://images.unsplash.com/photo-1597429926308-ffc8cd6f55fd?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=100&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTcwNDI1NjQ3Mw&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=100' };
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const json = await response.json();
+        setProducts(json);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) return <h1>one sec...</h1>;
+  if (error)
     return (
-        <>
-            <h1>Welcome to Shop!</h1>
-            <Product product={product} />
-        </>
+      <h1>
+        Oh no! <br /> {error}
+      </h1>
     );
+
+  return (
+    <>
+      <h1 id={styles.title}>the shop</h1>
+      <div id={styles.productContainer}>
+        {products.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
 }
