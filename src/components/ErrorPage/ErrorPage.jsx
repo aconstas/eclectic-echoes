48fce9a254b4a57a7fb34418fb6651dfd16302a6
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-02 22:20:35 -0800

    update error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import styles from "./ErrorPage.module.css";
 
 export default function ErrorPage() {
   return (
-    <>
+    <div id={styles.container}>
       <h1>Well shucks... this is awkward.</h1>
-      <Link to="home">go home</Link>
-    </>
+      <img src="https://i.giphy.com/l1J9EdzfOSgfyueLm.webp" /> <br />
+      <Link to="home" id={styles.link}>
+        go home
+      </Link>
+    </div>
   );
 }
