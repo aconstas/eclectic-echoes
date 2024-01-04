77c250cbe108924b503e8d51b06974970bf09ab1
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-03 21:09:16 -0800

    implement addToCart

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,15 +1,37 @@
+import { useState } from "react";
 import styles from "./Product.module.css";
 
-export default function Product({ product }) {
+export default function Product({ product, addToCart }) {
+  
+  const [quantity, setQuantity] = useState(0);
+  const [isAdded, setIsAdded] = useState(false);
+
+  function handleQuantityChange(event) {
+    setQuantity(event.target.value);
+  }
+
   return (
     <>
-      <div key={product.id} className={styles.container}>
+      <div className={styles.product}>
         <h2 className={styles.title}>{product.title}</h2>
         <div className={styles.price}>
-          <p>{product.price}</p>
-          <p>{product.rating.rate}/10</p>
+          <p>${product.price}</p>
+          <p>{product.rating.rate}/5</p>
         </div>
-        <img src={product.image} />
+        <img src={product.image} className={styles.prodImage} />
+        <br />
+        <form>
+          <input type="number" placeholder="quantity" onChange={handleQuantityChange}></input>
+          <button
+            className={isAdded ? styles.added : styles.addCart}
+            onClick={(event) => {
+              setIsAdded(true);
+              addToCart(event, product, quantity)
+            }}
+          >
+            {isAdded ? "Added!" : "Add to cart"}
+          </button>
+        </form>
       </div>
     </>
   );
