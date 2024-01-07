0045bbfe0c98169810f6cd89891160d66cbae9e5
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-07 11:50:56 -0800

    implement logic to render cart

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,28 @@
 import { Link } from "react-router-dom";
 import styles from "./NavBar.module.css";
+import cartIcon from "../../../public/cart-icon.png";
+import { useState } from "react";
+import Cart from "../Cart/Cart";
 
 export default function NavBar() {
+  const [showCart, setShowCart] = useState(false);
+
+  function handleCartClick() {
+    setShowCart(!showCart);
+  }
+
   return (
-    <header className={styles.navbar}>
-      <Link to="/home" className={styles.link}>HOME</Link>
-      <Link to="/shop" className={styles.link}>SHOP</Link>
-    </header>
+    <>
+      <header className={styles.navbar}>
+        <Link to="/home" className={styles.link}>
+          HOME
+        </Link>
+        <Link to="/shop" className={styles.link}>
+          SHOP
+        </Link>
+        <img src={cartIcon} alt="cart icon" onClick={handleCartClick} />
+      </header>
+      {showCart && <Cart />}
+    </>
   );
 }
