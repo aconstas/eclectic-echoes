227b7e234eb7ffaf86906af26d8c00e85320dd12
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-07 11:49:56 -0800

    add cart styles

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,11 @@
+import styles from './cart.module.css';
+
+export default function Cart() {
+    return(
+        <div id={styles.cartContainer}>
+            <h1>This is the cart.</h1>
+            <h1>This is the cart.</h1>
+            <h1>This is the cart.</h1>
+        </div>
+    )
+}
