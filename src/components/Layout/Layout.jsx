0bf5cb212897fcde35f6209d9499d67d10ea3332
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-07 16:30:45 -0800

    add cart state as props for NavBar

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,9 @@
 import NavBar from "../NavBar/NavBar";
 
-export default function Layout({ children }) {
+export default function Layout({ children, cart }) {
   return (
     <>
-      <NavBar />
+      <NavBar cart={cart} />
       {children}
     </>
   );
