b8d7be7bd763684b5991cca6cd0b14ee739d519c
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-08 19:47:23 -0800

    add 'Update' functionality to items previously added to cart

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,14 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./Product.module.css";
 
 export default function Product({ product, addToCart }) {
-  
   const [quantity, setQuantity] = useState(1);
   const [isAdded, setIsAdded] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [postClickDelay, setPostClickDelay] = useState(false);
 
   function handleQuantityChange(event) {
-    setQuantity(event.target.value);
+    setQuantity(Number(event.target.value));
   }
+
+  const handleMouseEnter = () => {
+    if (!postClickDelay) {
+      setIsHovered(true);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+
+  const handleAddToCart = (event) => {
+    setIsAdded(true);
+    setPostClickDelay(true);
+    addToCart(event, product, quantity);
+
+    setTimeout(() => {
+      setPostClickDelay(false);
+    }, 3000);
+  };
+
+  useEffect(() => {
+    // Cleanup timeout if component unmounts
+    return () => clearTimeout();
+  }, []);
 
   return (
     <>
@@ -18,19 +44,22 @@ export default function Product({ product, addToCart }) {
           <p>${product.price}</p>
           <p>{product.rating.rate}/5</p>
         </div>
-        <img src={product.image} className={styles.prodImage} loading="lazy"/>
+        <img src={product.image} className={styles.prodImage} loading="lazy" />
         <br />
         <form>
-          <input type="number" min="0" placeholder="quantity" onChange={handleQuantityChange}></input>
+          <input
+            type="number"
+            min="1"
+            placeholder="quantity"
+            onChange={handleQuantityChange}
+          ></input>
           <button
             className={isAdded ? styles.added : styles.addCart}
-            onClick={(event) => {
-              setIsAdded(true);
-              addToCart(event, product, quantity)
-            }
-          }
+            onClick={(event) => {handleAddToCart(event)}}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
           >
-            {isAdded ? "Added!" : "Add to cart"}
+            {isAdded ? (isHovered ? "Update?" : "Added!") : "Add to cart"}
           </button>
         </form>
       </div>
