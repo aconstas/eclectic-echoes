10df6aeb5772c179dcd2bb4d23a77eab4a9cb338
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-08 19:46:43 -0800

    pass cart to <Cart />

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
 import styles from "./NavBar.module.css";
-import cartIcon from "../../../src/assets/cart-icon.png";
+import cartIcon from "../../assets/cart-icon.png";
 import { useState } from "react";
-import Cart from '../Cart/Cart.jsx';
+import Cart from "../Cart/Cart.jsx";
 
-export default function NavBar({cart}) {
+export default function NavBar({ cart }) {
   const [showCart, setShowCart] = useState(false);
 
   function handleCartClick() {
     setShowCart((showCart) => !showCart);
   }
 
-  function totalItems (cart) {
+  function totalItems(cart) {
     let total = 0;
-    cart.forEach((item) => total += Number(item.quantity));
+    cart.forEach((item) => (total += Number(item.quantity)));
     return total;
   }
 
@@ -28,10 +28,10 @@ export default function NavBar({cart}) {
         </Link>
         <div id={styles.cartSection}>
           <img src={cartIcon} alt="cart icon" onClick={handleCartClick} />
-          <h2>{totalItems(cart)}</h2>
+          <h2 id={styles.totalItems}>{totalItems(cart)}</h2>
         </div>
       </header>
-      {showCart && <Cart />}
+      {showCart && <Cart cart={cart} />}
     </>
   );
 }
