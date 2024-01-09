0add41dfeffe3d5bf5b68b71ae97011d01718f40
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-08 19:48:04 -0800

    add cart info in <table>

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,35 @@
-import styles from './cart.module.css';
+import styles from "./cart.module.css";
 
-export default function Cart() {
-    return(
-        <div id={styles.cartContainer}>
-            <h1>This is the cart.</h1>
-            <h1>This is the cart.</h1>
-            <h1>This is the cart.</h1>
-        </div>
-    )
+export default function Cart({ cart }) {
+  const cartTotalPrice = (cart) => {
+    let total = 0;
+    cart.forEach((product) => {
+      total += product.price * product.quantity;
+    });
+    return total.toFixed(2);
+  };
+
+  return (
+    <div id={styles.cartContainer}>
+      <h1 id={styles.title}>your goods</h1>
+      <table>
+        <tr>
+          <th>Product</th>
+          <th>Quantity</th>
+          <th>Total</th>
+        </tr>
+        {cart.map((item) => (
+          <tr key={item.id}>
+            <td>{item.title}</td>
+            <td>{item.quantity}</td>
+            <td>${(item.quantity * item.price).toFixed(2)}</td>
+          </tr>
+        ))}
+      </table>
+      <div id={styles.grandTotalContainer}>
+        <span id={styles.grandTotal}>Grand Total: ${cartTotalPrice(cart)}</span>
+      </div>
+      <button id={styles.neuButton}>Checkout</button>
+    </div>
+  );
 }
