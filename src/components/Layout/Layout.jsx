95a0ee020ef87e5e7a9499a65c1b09b96b855c7b
aconstas <82304192+aconstas@users.noreply.github.com>
2024-01-08 19:46:55 -0800

    add <Footer />

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
-import NavBar from "../NavBar/NavBar";
+import NavBar from "../NavBar/NavBar.jsx";
+import Footer from "../Footer/Footer.jsx"
 
 export default function Layout({ children, cart }) {
   return (
     <>
       <NavBar cart={cart} />
       {children}
+      <Footer />
     </>
   );
 }
